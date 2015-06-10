538bf8ada4060d3f1cb693f878f6004468a403a2
mbradshawabs <mbradshaw@americanbible.org>
2015-06-10 14:28:32 -0500

    fix expected case for header names

--- test/index.js
+++ test/index.js
@@ -26,8 +26,8 @@ describe('parseAll()', function () {
     it('parses all Accept headers', function(done) {
 
         var headers = {};
-        headers['Accept-Charset'] = 'iso-8859-5, unicode-1-1;q=0.8, *;q=0.001';
-        headers['Accept-Encoding'] = 'compress;q=0.5, gzip;q=1.0';
+        headers['accept-charset'] = 'iso-8859-5, unicode-1-1;q=0.8, *;q=0.001';
+        headers['accept-encoding'] = 'compress;q=0.5, gzip;q=1.0';
         var accept = Accept.parseAll(headers);
         expect(accept.isBoom).to.not.exist();
         expect(accept).to.deep.equal({
