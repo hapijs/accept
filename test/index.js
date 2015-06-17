7e6786e6ac75592b7deee4f3fa6bace0ffe698a8
mbradshawabs <mbradshaw@americanbible.org>
2015-06-16 21:05:02 -0500

    expand parseAll() with media types

--- test/index.js
+++ test/index.js
@@ -26,6 +26,7 @@ describe('parseAll()', function () {
     it('parses all Accept headers', function(done) {
 
         var headers = {};
+        headers['accept'] = 'text/plain, application/json;q=0.5, text/html, */*;q=0.1';
         headers['accept-charset'] = 'iso-8859-5, unicode-1-1;q=0.8, *;q=0.001';
         headers['accept-encoding'] = 'compress;q=0.5, gzip;q=1.0';
         headers['accept-language'] = 'da, en;q=0.7, en-gb;q=0.8';
@@ -35,7 +36,8 @@ describe('parseAll()', function () {
         expect(accept).to.deep.equal({
             charsets: ['iso-8859-5', 'unicode-1-1', '*'],
             encodings: ['gzip', 'compress', 'identity'],
-            languages: ['da', 'en-gb', 'en']
+            languages: ['da', 'en-gb', 'en'],
+            mediaTypes: ['text/plain', 'text/html', 'application/json', '*/*']
         });
         done();
     });
